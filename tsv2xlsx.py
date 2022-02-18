071504197cbcd32b64d0637390031399220c11fc
crocodile <fat-crocodile@yandex.ru>
2022-02-19 02:41:25 +0300

    specifying arguments in code xlsx format changed a little

--- tsv2xlsx.py
+++ tsv2xlsx.py
@@ -2,6 +2,44 @@ import sys
 import xlsxwriter
 import re
 
+def main(argv):
+    if len(argv) != 2:
+        print("usage: %s tsvfile xlsxfile" % sys.argv[0])
+        return
+
+    with open(argv[0], 'r', encoding='utf-8') as f, xlsxwriter.Workbook(argv[1]) as wb:
+        bold = wb.add_format({'bold': True})
+        ws_header = wb.add_worksheet()
+        ws_body = wb.add_worksheet()
+        ws_body.set_column(1, 1, 50)
+        ws_body.set_column(3, 4, 70)
+        ws_body.write_row(0, 0, ["index", "before", "word", "after", "title"], cell_format=bold)
+        ws = WSwrapper(ws_body)
+        ws.append([])
+        wsh = WSwrapper(ws_header)
+        for i, line in enumerate(f):
+            line = line.strip()
+            if not line:
+                continue
+            if re.match("\w+=", line):
+                wsh.append(line.split('=', 1))
+            else:
+                parts = line.split('\t')
+
+                # index w0 w1 ... wn title
+                # all odd words are selected, all even are plain
+                if len(parts) < 5 or len(parts) % 2 != 1:
+                    raise Exception("line %d: wrong format" % (i + 1))
+
+                index = parts[0]
+                before = parts[1]
+                word = parts[2]
+                after = list(insert_x_before_odd_and_skip_empty(parts[3:-1], bold))
+                title = parts[-1]
+
+                ws.append([index, before, word, after, title])
+
+
 class WSwrapper:
     def __init__(self, ws):
         self.ws = ws
@@ -13,50 +51,23 @@ class WSwrapper:
             if isinstance(c, (list, tuple)):
                 if len(c) > 1:
                     self.ws.write_rich_string(self.line, i, *c)
-                else:
+                elif len(c) == 1:
                     self.ws.write(self.line, i, c[0])
             else:
                 self.ws.write(self.line, i, c)
         self.line += 1
 
 
-def insert_x_before_odd(data, x):
+def insert_x_before_odd_and_skip_empty(data, x):
     for i, d in enumerate(data):
+        if not d:
+            continue
         if i % 2 == 1:
             yield x
         yield d
 
 
-with open(sys.argv[1], 'r', encoding='utf-8') as f, xlsxwriter.Workbook(sys.argv[2]) as wb:
-    worksheet = wb.add_worksheet()
-    worksheet.set_column(1, 1, 50)
-    worksheet.set_column(3, 4, 70)
-    bold = wb.add_format({'bold': True})
-    ws = WSwrapper(worksheet)
 
-    header = True
-    for i, line in enumerate(f):
-        line = line.strip()
-        if not line:
-            continue
-        if re.match("\w+=", line):
-            if header:
-                ws.append(line.split('=', 1))
-        else:
-            if header and not ws.empty():
-                ws.append([])
-            header = False
-            parts = line.split('\t')
-
-            # index w0 w1 ... wn title
-            # all odd words are selected, all even are plain
-            if len(parts) < 5 or len(parts) % 2 != 1:
-                raise Exception("line %d: wrong format" % (i+1))
-
-            index = parts[0]
-            before = parts[1]
-            word = parts[2]
-            after = list(insert_x_before_odd(parts[3:-1], bold))
-            title = parts[-1]
-
-            ws.append([index, before, word, after, title])
+if __name__ == "__main__":
+    #main(sys.argv[1:])
+    main(["result.tsv", "result.xlsx"])
